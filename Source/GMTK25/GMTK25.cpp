aac7ec6a5dac6fbb2cefdcbb738db7c0e7d25073
wearenotwhoiam <weareryanmeyers@gmail.com>
2025-07-29 17:32:00 -0700

    First Commit

---
+++ Source/GMTK25/GMTK25.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GMTK25.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GMTK25, "GMTK25" );
